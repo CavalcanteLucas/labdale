69b761e772d3508038fffe9874e3f2f5f570182c
cavalcantelucas <lucas@labcodes.com.br>
2020-05-28 13:57:08 -0300

    Add tests for todo_list

--- todo_list/tests.py
+++ todo_list/tests.py
@@ -15,8 +15,11 @@ class TodoListTests(TestCase):
     def setUp(self):
         # Route
         self.url = reverse("todo_list:todo_list")
-        # Header
-        self.content_type = "application/json"
+
+    def test_list_todo_lists_requires_authorization(self):
+        client = Client()
+        response = client.get(path=self.url)
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_list_todo_lists(self):
         # Create user
@@ -25,8 +28,7 @@ class TodoListTests(TestCase):
 
         # Create todo list
         todo_list = TodoList.objects.create(
-            title=baker.prepare("TodoList").title,
-            owner=user
+            title=baker.prepare("TodoList").title, owner=user
         )
         self.assertEqual(1, TodoList.objects.count())
 
@@ -35,8 +37,9 @@ class TodoListTests(TestCase):
         self.assertTrue(created)
         client = Client(HTTP_AUTHORIZATION="Token " + token.key)
 
-        # Client asks for todo lists
+        # Client asks for todo lists successfully
         response = client.get(path=self.url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(1, len(response.data))
         self.assertEqual(todo_list.title, response.data[0]["title"])
         self.assertEqual(todo_list.owner.pk, response.data[0]["owner"])
@@ -48,10 +51,7 @@ class TodoListTests(TestCase):
         self.assertEqual(2, User.objects.count())
 
         # Create todo list with user_1 as owner
-        todo_list = TodoList.objects.create(
-            title=baker.prepare("TodoList").title,
-            owner=user_1
-        )
+        TodoList.objects.create(title=baker.prepare("TodoList").title, owner=user_1)
         self.assertEqual(1, TodoList.objects.count())
 
         # Authenticate user_2
@@ -61,8 +61,8 @@ class TodoListTests(TestCase):
 
         # Client asks for todo lists, none should be available
         response = client.get(path=self.url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(0, len(response.data))
-
 
     def test_create_todo_list(self):
         # Create user
@@ -74,16 +74,12 @@ class TodoListTests(TestCase):
         self.assertTrue(created)
         client = Client(HTTP_AUTHORIZATION="Token " + token.key)
 
-        # Client creates todo list
-        todo_list_sample = {
-            "title": baker.prepare("TodoList").title,
-            "owner": user.pk
-        }
-        client.post(
-            path=self.url,
-            content_type=self.content_type,
-            data=todo_list_sample
+        # Client creates todo list successfully
+        todo_list_sample = {"title": baker.prepare("TodoList").title, "owner": user.pk}
+        response = client.post(
+            path=self.url, content_type="application/json", data=todo_list_sample
         )
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(1, TodoList.objects.count())
         self.assertEqual(todo_list_sample["title"], TodoList.objects.get(id=1).title)
         self.assertEqual(todo_list_sample["owner"], TodoList.objects.get(id=1).owner.pk)
