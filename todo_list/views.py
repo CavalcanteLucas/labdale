061ab76385d40be236297b9cfd16e615c28a74e7
cavalcantelucas <lucas@labcodes.com.br>
2020-06-05 12:34:08 -0300

    Refactor TodoList and TodoListDetail APIViews

--- todo_list/views.py
+++ todo_list/views.py
@@ -18,32 +18,13 @@ class TodoListAPIView(generics.ListCreateAPIView):
         user = self.request.user
         return TodoList.objects.filter(owner=user).order_by("-id")
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        data.update({"owner": request.user.pk})
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class TodoListDetailAPIView(generics.RetrieveUpdateAPIView):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
-    def update(self, request, *args, **kwargs):
-        data = request.data
-        data.update({"owner": request.user.pk})
-
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response(serializer.data)
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
