c8088d7f8d89a04300070b3694f876d21639fe9b
cavalcantelucas <lucas@labcodes.com.br>
2020-06-05 14:03:38 -0300

    Adding timezone to created_at field in TodoListSerializer

--- todo_list/serializers.py
+++ todo_list/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import TodoList
 from datetime import datetime
+from django.utils import timezone
 
 class TodoListSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     owner = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
-    created_at = serializers.DateTimeField(default=datetime.now)
+    created_at = serializers.DateTimeField(default=datetime.now(tz=timezone.utc))
 
     def create(self, validated_data):
         return TodoList.objects.create(**validated_data)
