79f6c615f3d7abd7cc008de51ec3c61dfb318480
cavalcantelucas <lucas@labcodes.com.br>
2020-06-19 20:14:15 -0300

    Add requirements for Todo tests

---
+++ labdale/todo_lists/tests/test_todos.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class TodoTests(TestCase):
+    def test_todo__str__(self):
+        pass
+
+    def test_create_todo_requires_authorization(self):
+        pass
+
+    def test_create_todo_is_private(self):
+        pass
+
+    def test_create_todo_requires_data(self):
+        pass
+
+    def test_create_todo(self):
+        pass
